aeba46e2a19451a45ae28542fce468df124ad365
Sam Fan <samisfan@msn.com>
2020-12-08 11:43:31 -0700

    server.js and html routes half working, index works, working on notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,13 @@
 const router = require("express").Router();
+const nanoid = require("nanoid");
+
+// GET for api/notes
+router.get("/api/notes", (req, res) => res.json(notes));
+// POST for api/notes
+router.post("/api/notes", (req, res) => res.json(notes));
+// something here to display notes
+
+// DELETE for api/notes
+router.delete("/api/notes", (req, res) => res.json(notes));
 
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,15 @@
 const router = require("express").Router();
 const path = require("path");
+const nanoid = require("nanoid");
+
+// GET home
+router.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+);
+
+// GET for notes.html
+router.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/notes.html"))
+);
 
 module.exports = router;
