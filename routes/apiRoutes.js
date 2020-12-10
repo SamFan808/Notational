acaa83627c3b0c9900bfb67739cc0ef9068fd1cc
Sam Fan <samisfan@msn.com>
2020-12-09 19:40:46 -0700

    working out how to write to db.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,25 @@
 const router = require("express").Router();
 const nanoid = require("nanoid");
 const path = require("path");
+// const bodyParser = require("body-parser");
+// const express = require("express");
+// const app = express();
+// app.use(bodyParser.json());
 
 // GET for api/notes
 router.get("/api/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "../db/db.json"))
 );
 
-//
-
 // POST for api/notes
-router.post("/api/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "../db/db.json"))
-);
+router.post("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../db/db.json"));
+  console.log(req.body);
+});
+
+// router.post("/api/notes", (req, res) =>
+//   res.sendFile(path.join(__dirname, "../db/db.json"))
+// );
 // something here to display notes?
 
 // DELETE for api/notes
