58fcc6d0b2de6ba0f3b437b4f31063f374303a7e
Sam Fan <samisfan@msn.com>
2020-12-11 17:59:19 -0700

    fixed buggy given code, index.js need a few tweaks, everything working as expected now

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -61,6 +61,8 @@ const renderActiveNote = () => {
   } else {
     noteTitle.value = "";
     noteText.value = "";
+    noteTitle.removeAttribute("readonly"); // added to allow for text to allow for new note
+    noteText.removeAttribute("readonly"); // added to allow for text to allow for new note
   }
 };
 
